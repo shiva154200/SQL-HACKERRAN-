8490714e68fed3b5df9d1f2cd66e125952df6541
Shivam Jogdand <shivamjogdand1458@gmail.com>
2026-09-06 11:33:13 +0530

    Time: 40 ms (14.97%), Space: 26.8 MB (72.62%) - CodeHub

--- leetcode/0115-distinct-subsequences/0115-distinct-subsequences.cpp
+++ leetcode/0115-distinct-subsequences/0115-distinct-subsequences.cpp
@@ -1,23 +1,37 @@
 class Solution {
 public:
-    int helper(string & s, string & t, vector<vector<int>>&dp,int si,int ti){
-        if(ti==t.size()) return 1;
-        
-        if(t.length()-ti>s.length()-si) return 0;
-        int ans=0;
-       for(int i=si;i<s.length();i++){
-        if(s[i]==t[ti]) {
-            if(dp[i][ti]==-1) dp[i][ti]=helper(s,t,dp,i+1,ti+1);
-            ans+=dp[i][ti];
+    int helper(string &s, string &t, vector<vector<int>>& dp, int si, int ti) {
+     
+        if (ti == t.size())
+            return 1;
 
+      
+        if (si == s.size())
+            return 0;
+
+       
+        if (dp[si][ti] != -1)
+            return dp[si][ti];
+
+        if (s[si] == t[ti]) {
+            int take = helper(s, t, dp, si + 1, ti + 1);
+            int skip = helper(s, t, dp, si + 1, ti);
+
+            return dp[si][ti] = take + skip;
         }
-       }
-       return ans;
+
+        return dp[si][ti] = helper(s, t, dp, si + 1, ti);
     }
+
     int numDistinct(string s, string t) {
-        if(t.length()>s.length()) return 0;
-        vector<vector<int>>dp(s.length(),vector<int>(t.length(),-1));
-        return helper(s, t,dp,0,0);
-        
+        if (t.length() > s.length())
+            return 0;
+
+        vector<vector<int>> dp(
+            s.length(),
+            vector<int>(t.length(), -1)
+        );
+
+        return helper(s, t, dp, 0, 0);
     }
 };
