363392d73d2cf0c0e9c1c1e41dfb5f33bc102088
Shivam Jogdand <shivamjogdand1458@gmail.com>
2026-09-09 10:36:14 +0530

    Time: 0 ms (100%), Space: 9.1 MB (19.2%) - CodeHub

---
+++ leetcode/4248-count-commas-in-range-ii/4248-count-commas-in-range-ii.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    long long countCommas(long long n) {
+        if(n<1000) return 0;
+        int m=1000;
+        long long t=999;
+        long long ans=0;
+        long long z=999;
+        int d=0;
+        while(n>z){
+             
+            ans+=t*d;
+            t*=1000;
+            z+=t;
+         
+            d++;
+        }
+        z-=t;
+        cout<<z;
+        ans+=((n-z)*d);
+        return ans;
+
+        
+    }
+};
